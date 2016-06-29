6a3a5b41d1f346cf433c741cf76cc7eb41a194d5
Anish Singh <singh.anish.home@gmail.com>
2016-06-29 00:45:36 -0400

    .

--- src/ch1/MiscQuestions.java
+++ src/ch1/MiscQuestions.java
@@ -69,30 +69,77 @@ public class MiscQuestions {
 	}
 	
 	public int [][] clearRowsAndColumns(int [][] x) {
-		//copy the matrix
-		int [][] y = new int [x.length][];
-		for (int i=0;i<x.length;i++)
-		{
-			y[i] = new int[x[i].length];
-			for (int j=0; j<x[i].length;j++)
-				y[i][j] = x[i][j];
-		}
 		
+		
+		boolean firstRowHasZero = false;
+		boolean firstColHasZero = false;
+		
+		
+		// for i =0 to x.length. if c[i][0] == 0 for j=0 to c[i].length c[i][j]=0
+		// for j= 0 to x[0].length if c[0][j] == 0 for i=0 to x.length c[i][j] = 0
+		
+		// if a cell (i,j) has 0 then set c(i,0) = 0 and c(0,j) = 0;
 		for(int i=0;i<x.length;i++)
 		{
-			for (int j=0;j<x.length;j++)
+			for (int j=0;j<x[i].length;j++)
 			{
 				if(x[i][j] == 0) {
-					for(int c=0; c<y[i].length;c++)
-						y[i][c] = 0;
-					for(int r=0; r<y.length;r++)
-						y[r][j] = 0;
+					if(i==0) firstRowHasZero = true;
+					if(j==0) firstColHasZero = true;
+					x[i][0] = 0;
+					x[0][j] = 0;
 				}
 			}
 		}
-		return y;
+
+		//for row 2 to last. if first col is 0 then set 2nd col to last col to 0
+		for (int i=1; i < x.length;i++){
+			if(x[i][0] == 0) {
+				for(int j=1; j<x[i].length;j++)
+					x[i][j] = 0;
+			}
+		}
+		
+		//for col 2 to last. if first row is 0 then set 2nd row to last row to 0.
+		for (int j=1; j < x[0].length;j++){
+			if(x[0][j] == 0) {
+				for(int i=1; i< x.length;i++)
+					x[i][j] = 0;
+			}
+		}
+		
+		//if first col or row had zeros. then set it to zero.
+		if(firstColHasZero) {
+			for (int i=0; i<x[0].length;i++)
+				x[i][0] = 0;
+		}
+		if(firstRowHasZero) {
+			for (int i=0; i<x[0].length;i++)
+				x[0][i] = 0;
+		}
+			
+		return x;
 	}
 	
+	
+	public int[][] rotateRight(int[][] x) {
+		int N = x.length;
+		int numLayers = N/2;
+
+		for(int l=1; l<=numLayers;l++) {
+			int startIdx = l -1;
+			int endIdx = N-l;
+
+			for(int i=startIdx; i<=endIdx;i++) {
+				int top = x[l-1][i];
+				x[l-1][i] = x[N-l-i][i];
+				x[N-l-i][i] = x[N-l-i][N-l-i];
+				x[N-l-i][N-l-i] = x[l-1][N-l-i];
+				x[l-1][N-l-i] = top;
+			}
+		}
+		return x;
+	}
 	
 	public void printMatrix(int [][] x)
 	{
@@ -101,6 +148,7 @@ public class MiscQuestions {
 			for(int j=0;j<x[i].length; j++) 
 				System.out.print(x[i][j]);
 		}
+		System.out.println("\n----");
 			
 	}
 	
@@ -110,10 +158,11 @@ public class MiscQuestions {
 //		System.out.println(q.hasUniqueCharacters("abcb"));
 //		System.out.println(q.replaceSpace(" a b cd      "));
 //		System.out.println(q.replaceSpace(" a   "));
-		int [][] m = new int[][] { {1,2,0}, {1,0,3}, {3,2,1}};
+		int [][] m = new int[][] { {1,2,3}, {4,5,6}, {7,8,9}};
+//		int [][] m = new int[][] { {1,2},{3,4}};
 		q.printMatrix(m);
-		System.out.println("----");
-		int [][] r = q.clearRowsAndColumns(m);
+		
+		int [][] r = q.rotateRight(m);
 		q.printMatrix(r);
 
 	}
